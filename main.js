012234e102a5e1104c2ea39e6c086e0e412c77af
Andrew W <woodygoalie007@gmail.com>
2023-04-04 19:58:19 -0600

    Added light and dark theme toggle
    Added the light and dark theme toggle, need to move this toggle to the header on the right hand side

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-function expandAccordionWithParents(accordionId) {
-  var accordion = document.getElementById(accordionId);
-  accordion.classList.add("active");
-  var panel = accordion.nextElementSibling;
-  panel.style.maxHeight = panel.scrollHeight + "px";
-  
-  var parent = accordion.parentElement;
-  while (parent && parent.tagName !== "BODY") {
-    if (parent.classList.contains("panel")) {
-      parent.style.maxHeight = parent.scrollHeight + "px";
-      parent.previousElementSibling.classList.add("active");
-    }
-    parent = parent.parentElement;
-  }
-}
+
+const toggle = document.getElementById("toggle");
+const theme = window.localStorage.getItem("theme");
+
+/* verifica se o tema armazenado no localStorage é escuro
+se sim aplica o tema escuro ao body */
+if (theme === "dark") document.body.classList.add("dark");
+
+// event listener para quando o botão de alterar o tema for clicado
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (theme === "dark") {
+    window.localStorage.setItem("theme", "light");
+  } else window.localStorage.setItem("theme", "dark");
+});
